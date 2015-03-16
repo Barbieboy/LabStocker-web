8a0dfca9f9c7682bdd04bd50dcd82c3d417d7cda
Álex Filipe <alexfilipe@me.com>
2015-03-16 13:20:08 -0300

    separar sidebar

---
+++ sidebar.php
@@ -0,0 +1,49 @@
+<nav class="navbar-default sidebar" role="navigation">
+            <div class="sidebar-nav navbar-collapse">
+                <ul class="nav" id="side-menu">
+                    <li class="sidebar-search">
+                        <div class="input-group custom-search-form">
+                            <input type="text" class="form-control" placeholder="Pesquisar...">
+                            <span class="input-group-btn">
+                            <button class="btn btn-default" type="button">
+                                <i class="fa fa-search"></i>
+                            </button>
+                        </span>
+                        </div>
+                        <!-- /input-group -->
+                    </li>
+                    <li class="active">
+                        <a href="#"><i class="fa fa-flask"></i> Reagentes <span class="fa arrow fa-2x"></span></a>
+                        <ul class="nav nav-second-level">
+                            <li>
+                                <a href="pesquisarReagente.php">Pesquisar Reagente</a>
+                            </li>
+                            <li>
+                                <a href="cadastrarReagente.php">Cadastrar Reagente</a>
+                            </li>
+                            <li>
+                                <a href="cadastrarFabricante.php">Cadastrar Fabricante</a>
+                            </li>
+                        </ul>
+                        <!-- /.nav-second-level -->
+                    </li>
+                    <li class="active">
+                        <a href="#"><i class="fa fa-users"></i> Usuários <span class="fa arrow fa-2x"></span></a>
+                        <ul class="nav nav-second-level">
+                            <li>
+                                <a href="cadastrarUsuario.php">Cadastrar Usuário</a>
+                            </li>
+                            <li>
+                                <a href="pesquisarUsuario.php">Pesquisar Usuário</a>
+                            </li>
+                            <li>
+                                <a href="cadastrarGrupo.php">Cadastrar Grupo</a>
+                            </li>
+                        </ul>
+                        <!-- /.nav-second-level -->
+                    </li>
+                </ul>
+            </div>
+            <!-- /.sidebar-collapse -->
+        </nav>
+        <!-- /.navbar-static-side -->
